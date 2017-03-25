240ff0c5f76892d611968188d9f31d01d9a4c9e1
kristalys47 <kristalys.cba4@yahoo.com>
2017-03-25 19:25:22 -0400

    Grid 9x9, numeros, colores, bombas, flags. CASI TODO menos lo que falta.BYE 4:23am jajaja 26/marzo/2017 y ahora no me deja subirlo HAY DIOS

--- Projecto/src/Main.java
+++ Projecto/src/Main.java
@@ -1,18 +1,26 @@
 import javax.swing.JFrame;
-//just trying
+
 public class Main {
 	public static void main(String[] args) {
+	
 		JFrame myFrame = new JFrame("Color Grid");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(400, 150);
 		myFrame.setSize(400, 400);
-
 		MyPanel myPanel = new MyPanel();
 		myFrame.add(myPanel);
-
 		MyMouseAdapter myMouseAdapter = new MyMouseAdapter();
 		myFrame.addMouseListener(myMouseAdapter);
-
 		myFrame.setVisible(true);
 	}
+
 }
+//0
+
+/* Que falta? 
+ * falta que abran todos los espacios grises que no tienes numero si estan pegados
+ * falta que si explotan una mine se revelan todas
+ * falta verificar el numero de minas que se esta exigiendo 
+ * dar una ultima revisada a los requisitos y creo que ya. jejeje
+ * ELIMINAR TODOS LOS COMMENTS RAROS
+ */
